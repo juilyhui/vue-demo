d4b36fe05a0a9584b23db7c0483ee1e97d865daa
金静慧 <jinjinghui@jinjinghuideMacBook-Pro.local>
2016-09-05 12:19:50 +0800

    hui 弹框组件示例

--- component/main.js
+++ component/main.js
@@ -1,12 +1,36 @@
 /*
 * 组件
+* 组件可以扩展HTML元素，封装可重用的HTML代码，我们可以将组件看作自定义的HTML元素。
+* 组件的使用步骤：
+  1、创建组件构造器  Vue.extend()
+  2、注册组件  Vue.component()
+  3、使用组件
 */
 
-// 定义
+/*
+理解组件的创建和注册：
+1. Vue.extend()是Vue构造器的扩展，调用Vue.extend()创建的是一个组件构造器，而不是一个具体的组件实例。
+2. Vue.extend()构造器有一个选项对象，选项对象的template属性用于定义组件要渲染的HTML。
+3. 使用Vue.component()注册组件时，需要提供2个参数，第1个参数时组件的标签，第2个参数是组件构造器。
+4. Vue.component()方法内部会调用组件构造器，创建一个组件实例。
+5. 组件应该挂载到某个Vue实例下，否则它不会生效。
+*/
+
+
+/*
+* 全局注册和局部注册
+* 调用Vue.component()注册组件时，组件的注册是全局的，这意味着该组件可以在任意Vue示例下使用。
+* 如果不需要全局注册，或者是让组件使用在其它组件内，可以用选项对象的components属性实现局部注册。
+*/
+
+/*
+* 全局注册
+*/
+// 定义，创建组件构造器 第一步
 var MyComponent = Vue.extend({
     template: '<div>A custom compent!</div>'
 })
-// 注册
+// 注册 第二步
 Vue.component('my-component', MyComponent);
 
 // 在一个步骤中扩展与注册，将上面两个个步骤合并的写法
@@ -14,7 +38,7 @@ Vue.component('my-component', MyComponent);
 //     template: '<div>A custom component!</div>'
 // })
 
-// 创建根实例
+// 创建根实例   第三步
 new Vue({
     el: "#example"
 })
@@ -24,35 +48,114 @@ new Vue({
 /*
 * 局部注册
 */
-var Child = Vue.extend({
-    // ...
+// 1.创建一个组件构造器
+var MyComponent2 = Vue.extend({
+    template: '<div>this is my first component!</div>'
 })
-var Parent = Vue.extend({
-    template: '...',
+// 2. 将myComponent组件注册到Vue实例下，只有在#app元素下会被使用
+new Vue({
+    el: '#app',
     component: {
-        // <my-component> 只能用在父组件模板内
-        'my-component': Child
+        'my-component': MyComponent
     }
 })
+// var Child = Vue.extend({
+//     // ...
+//     template: '<div>这是局部注册</div>'
+// })
+// var Parent = Vue.extend({
+//     el: "#app",
+//     // template: '...',
+//     component: {
+//         // <my-component> 只能用在父组件模板内
+//         // my-component组件是注册在#app元素对应的Vue实例下的，所以它不能在其它Vue实例下使用。
+//         'my-component': Child
+//     }
+// })
+
 
 
 /*
 * 注册语法糖
 */
+// Vue.component()的第1个参数是标签名称，第2个参数是一个选项对象，使用选项对象的template属性定义组件模板。
 
-// 在一个步骤中扩展与注册
+// 1、使用Vue.component()直接创建和注册组件：  ---全局注册---
+//  my-component 是html标签名称
 Vue.component('my-component', {
     template: '<div>A custom component!</div>'
 })
+new Vue({
+   el: '#app1'
+})
+// // 局部注册也可以这么做
+// var Parent = Vue.extend({
+//     component: {
+//         'my-component': {
+//             template: '<div>A custom component!</div>'
+//         }
+//     }
+// })
 
-// 局部注册也可以这么做
-var Parent = Vue.extend({
+// 2、在选项对象的components属性中实现局部注册：
+var vm2 = new Vue({
+    el: '#app2',
     component: {
-        'my-component': {
-            template: '<div>A custom component!</div>'
+        // 局部注册，my-component2是标签名称
+        'my-component2': {
+            template: '<div>this is the second component</div>'
+        },
+        // 局部注册，my-component3是标签名称
+        'my-component3': {
+            template: '<div>this is the third component</div>'
         }
     }
 })
+
+// 3、使用script或template标签     ---------建议使用---------
+// 尽管语法糖简化了组件注册，但在template选项中拼接HTML元素比较麻烦，这也导致了HTML和JavaScript的高耦合性。
+// 庆幸的是，Vue.js提供了两种方式将定义在JavaScript中的HTML模板分离出来。
+
+// ---使用script---
+/*
+<div id="app">
+    <my-component></my-component>
+</div>
+
+// 使用<script>标签时，type指定为text/x-template，意在告诉浏览器这不是一段js脚本，浏览器在解析HTML文档时会忽略<script>标签内定义的内容。
+<script type="text/x-template" id="myComponent">
+    <div>This is a component!</div>
+</script>
+*/
+Vue.component('my-component', {
+    template: '#myComponent'  // template选项现在不再是HTML元素，而是一个id。Vue.js根据这个id查找对应的元素，然后将这个元素内的HTML作为模板进行编译。
+})
+new Vue({
+    el: '#app'
+})
+
+// ---使用template---
+// 不需要指定type属性。
+// IE不支持<template>标签，这使得在IE中<template>标签中的内容会显示出来。解决办法——隐藏<template>标签
+/*
+<div id="app">
+    <my-component></my-component>
+</div>
+
+<template id="myComponent">
+    <div>This is a component!</div>
+</template>
+*/
+Vue.component('my-component',{
+   template: '#myComponent'
+})
+new Vue({
+   el: '#app'
+})
+
+
+
+
 
 /*
 * 组件选项问题
@@ -82,19 +185,61 @@ var MyComponent = Vue.extend({
 */
 
 /*
-* 使用Props传递数据
+* 使用Props传递数据，传给子组件
 * 组件实例的作用域是孤立的。这意味着不能并且不应该在子组件的模板内直接引用父组件的数据。可以使用 props 把数据传给子组件。
 * 名字形式为 camelCase 的 prop 用作特性时，需要转为 kebab-case（短横线隔开）
 */
 
 // “prop” 是组件数据的一个字段，期望从父组件传下来。子组件需要显式地用 props 选项 声明 props：
-Vue.component('child', {
-    // 声明 props
-    props: ['msg'],
-    // prop 可以用在模板内
-    // 可以用 `this.msg` 设置
-    template: '<span>{{msg}}</span>'
+// Vue.component('child', {
+//     // 声明 props
+//     props: ['msg'],
+//     // prop 可以用在模板内
+//     // 可以用 `this.msg` 设置
+//     template: '<span>{{msg}}</span>'
+// })
+
+
+// prps基础示例：
+// 可以将这个Vue实例看作my-component的父组件。
+// 如果我们想使父组件的数据，则必须先在子组件中定义props属性，也就是props: ['myName', 'myAge']这行代码。
+var vm = new Vue({
+    el: '#app',
+    data: {
+        name: 'juily',
+        age: 21
+    },
+    component: {
+        'my-component': {
+            template: '#MyComponent',
+            props: ['myName', 'myAge']
+        }
+    }
 })
+// 定义子组件的HTML模板：
+/*
+<template id="myComponent">
+    <div>
+        <div>my name</div>
+        <div>{{ myName }}</div>
+    </div>
+    <div>
+        <div>my age</div>
+        <div>{{ myAge }}</div>
+    </div>
+</template>
+*/
+// 将父组件数据通过已定义好的props属性传递给子组件：
+/*
+<div id="app">
+    <my-component v-bind:my-name="name" v-bind:my-age="age"></my-component>
+</div>
+*/
+
+// 在父组件中使用子组件时，通过以下语法将数据传递给子组件：
+// <child-component v-bind:子组件prop="父组件数据属性"></child-component>
+
+
 
 /*
 * 动态Props
@@ -104,7 +249,8 @@ Vue.component('child', {
 * Props 绑定类型
 * 默认为但想绑定: 父组件的属性变化时，将传给子组件。
 * .sync 双向绑定
-* .once 单次绑定
+* .once 单次绑定  即使父组件修改了数据，也不会传导给子组件。
+<my-component v-bind:my-name.sync="name" v-bind:my-age.sync="age"></my-component>
 */
 
 /*
@@ -162,6 +308,44 @@ Vue.component('example', {
     }
 })
 
+
+/*
+* 父组件和子组件
+* 可以在组件中定义并使用其他组件，这就构成了父子组件的关系。
+* 下面两种子组件的使用方式是错误的：
+  1. 以子标签的形式在父组件中使用:
+    <div id="app">
+        <parent-component>
+          <child-component></child-component>
+        </parent-component>
+    </div>
+  2. 在父组件标签外使用子组件:
+      <div id="app">
+          <parent-component>
+          </parent-component>
+          <child-component>
+          </child-component>
+      </div>
+*/
+var Child = Vue.extend({
+    template: '<p>this is child component!</p>'
+})
+var Parent = Vue.extend({
+    // 在Parent组件内使用<child-component>标签
+    template: '<p>This is a Parent component</p><child-component></child-component>',
+    component: {
+        // 局部注册Child组件，该组件只能在Parent组件内使用
+        'child-component': Child
+    }
+})
+// 全局注册Parent组件
+Vue.component('parent-component', Parent);
+new Vue({
+    el: '#app'
+})
+
+
+
 /*
 * 父子组件通信
 */
@@ -209,6 +393,8 @@ var parent = new Vue({
 
 /*
 * 使用Slot分发内容
+* 为了让组件可以组合，我们需要一种方式来混合父组件的内容与子组件自己的模板。这个处理称为内容分发，Vue.js 实现了一个内容分发 API，使用特殊的 <slot> 元素作为原始内容的插槽。
+* <slot>元素是一个内容插槽。
 */
 
 
